cd57c1246764e8bcef74a4d88190dcab3a4cb41f
Phong Hoang <hoangnghiaphong_k64a4>
2022-12-21 13:17:46 +0700

    create ui password

---
+++ app/view/password_request.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <center style="
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+">
+        <form method="POST">
+            <div>
+                <label>Người dùng</label>
+                <input style="margin-left: 10px;" type="text" name="login_id" id="">
+            </div>
+            <div style="display: flex; justify-content: end;">
+                <button
+                    style="margin-top: 20px; background-color: #5392f8; height: 30px; cursor: pointer; border-style: none; border-radius: 4px;">
+                    <span>Gửi yêu cầu reset password</span>
+                </button>
+            </div>
+        </form>
+    </center>
+</body>
+
+</html>
