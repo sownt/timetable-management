2008368d066dcca89530dfa7780dbf31fb650463
nguyenminhquang_k64a4 <nguyenminhquang1_t64@gmail.com>
2022-12-22 00:01:52 +0700

    create teacher add complete

---
+++ app/view/teacher_add_complete.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Complete</title>
+    <link rel="stylesheet" href="../../web/css/teacher_add_complete.css">
+</head>
+<body>
+<?php
+session_start();
+?>
+
+<div class="container">
+    <div class="noti">
+        Bạn đã đăng ký thành công giáo viên.
+    </div>
+    <div class="backtohome">
+        <a href="<blank>.php" target="_self" style="color: black; ">Trở về trang chủ</a>
+    </div>
+</div>
+</body>
+</html>
