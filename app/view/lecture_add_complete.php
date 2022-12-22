c1682c16bc4a12b224c460b420fdeee5ff3e812d
Son Tran Thai <thonsai@outlook.com>
2022-12-22 12:52:28 +0700

    Add New Lecture page UI
    Add New lecture page complete, confirm and input

---
+++ app/view/lecture_add_complete.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>New Lecture | Successful</title>
+</head>
+
+<body>
+    <div class="container text-center">
+        <h3>Bạn đã đăng ký thành công môn học.</h3>
+        <a href="index.php?controller=lecture&action=lectureList"><u>Trở về Trang chủ</u></a>
+    </div>
+</body>
+
+</html>
