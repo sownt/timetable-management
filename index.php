40b66b4a21cf9e4c7f4ccc1494ee25b7e135d181
Son Tran Thai <thonsai@outlook.com>
2022-12-28 00:42:57 +0700

    Refactor code

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$controller = isset($_GET['controller']) ? $_GET['controller'] . 'Controller' : 'UserController';
-$action = isset($_GET['action']) ? $_GET['action']: 'getUser';
-
-require_once('./app/controller/UserController.php');
-$usercontroller = new $controller();
-$usercontroller -> $action();
-
-?>
