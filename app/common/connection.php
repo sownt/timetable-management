735c10744811db8328ec33c099c5056f04b73862
Son Tran Thai <thonsai@outlook.com>
2023-01-15 17:16:33 +0700

    Update db

---
+++ app/common/connection.php
@@ -0,0 +1,16 @@
+<?php
+class DB
+{
+    private static $instance = NULl;
+    public static function getInstance() {
+      if (!isset(self::$instance)) {
+        try {
+          self::$instance = new PDO('mysql:host=10.0.1.2;dbname=timetable_management', 'root', 'my-secret-pw');
+          self::$instance->exec("SET NAMES 'utf8'");
+        } catch (PDOException $ex) {
+          die($ex->getMessage());
+        }
+      }
+      return self::$instance;
+    }
+}
--- app/common/timetable.sql
+++ app/common/timetable.sql
@@ -1,24 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
 --
--- Cấu trúc bảng cho bảng `admins`
+-- Host: db
+-- Generation Time: Jan 15, 2023 at 10:00 AM
+-- Server version: 10.9.3-MariaDB-1:10.9.3+maria~ubu2204
+-- PHP Version: 8.0.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `timetable_management`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
 --
 
 CREATE TABLE `admins` (
-  `id` int(10) NOT NULL PRIMARY KEY,
+  `id` int(10) NOT NULL,
   `login_id` varchar(20) DEFAULT NULL,
   `password` varchar(64) DEFAULT NULL,
   `actived_flag` int(1) DEFAULT NULL,
   `reset_password_token` varchar(100) DEFAULT NULL,
   `updated` datetime DEFAULT NULL,
   `created` datetime DEFAULT NULL
-) 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `schedules`
+-- Table structure for table `schedules`
 --
 
 CREATE TABLE `schedules` (
-  `id` int(10) NOT NULL PRIMARY KEY,
+  `id` int(10) NOT NULL,
   `school_year` char(10) DEFAULT NULL,
   `subject_id` int(10) DEFAULT NULL,
   `teacher_id` int(10) DEFAULT NULL,
@@ -27,32 +53,32 @@ CREATE TABLE `schedules` (
   `notes` text DEFAULT NULL,
   `updated` datetime DEFAULT NULL,
   `created` datetime DEFAULT NULL
-)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `subjects`
+-- Table structure for table `subjects`
 --
 
 CREATE TABLE `subjects` (
-  `id` int(10) NOT NULL PRIMARY KEY,
+  `id` int(10) NOT NULL,
   `name` varchar(250) DEFAULT NULL,
   `avatar` varchar(250) DEFAULT NULL,
   `description` text DEFAULT NULL,
   `school_year` char(10) DEFAULT NULL,
   `updated` datetime DEFAULT NULL,
   `created` datetime DEFAULT NULL
-) 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `teachers`
+-- Table structure for table `teachers`
 --
 
 CREATE TABLE `teachers` (
-  `id` int(10) NOT NULL PRIMARY KEY,
+  `id` int(10) NOT NULL,
   `name` varchar(250) DEFAULT NULL,
   `avatar` varchar(250) DEFAULT NULL,
   `description` text DEFAULT NULL,
@@ -60,4 +86,37 @@ CREATE TABLE `teachers` (
   `degree` char(10) DEFAULT NULL,
   `updated` datetime DEFAULT NULL,
   `created` datetime DEFAULT NULL
-)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `schedules`
+--
+ALTER TABLE `schedules`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `subjects`
+--
+ALTER TABLE `subjects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `teachers`
+--
+ALTER TABLE `teachers`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
