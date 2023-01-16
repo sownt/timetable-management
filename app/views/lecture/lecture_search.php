d2271db4692f85083d2e79e0064e4b9774060de8
TaAnhQuan <77721385+TaAnhQuan@users.noreply.github.com>
2023-01-16 14:01:49 +0700

    update lecture_search

--- app/views/lecture/lecture_search.php
+++ app/views/lecture/lecture_search.php
@@ -6,7 +6,98 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel='stylesheet' href='../../web/css/lecture_search.css'>
+    <link rel='stylesheet' href='lecture_search.css'>
+    <style type='text/css'>
+        .td {
+            padding: 10px 15px 5px 35px;
+        }
+
+        table {
+            border-spacing: 10px;
+        }
+
+        .box {
+            border: 2px solid steelblue;
+            line-height: 17px;
+            padding: 3px 0px 5px 0px;
+        }
+
+        #input {
+            width: 165px;
+        }
+
+        button {
+            width: 110px;
+            height: 40px;
+            margin-top: 10px;
+            background-color: steelblue;
+            font-size: 14px;
+            font-family: 'Times New Roman', Times, serif;
+            font-weight: normal;
+            color: white;
+            border: 3px solid rgb(32, 106, 163);
+            border-radius: 8px;
+            margin-left: 150px;
+            text-align: center;
+        }
+
+        .result {
+            display: flex;
+            text-align: left;
+            flex-direction: row;
+            margin-left: 10px;
+            margin-top: 15px;
+            justify-content: space-between;
+        }
+
+        .btn_add {
+            width: 50px;
+            height: 20px;
+            padding-top: 3px;
+            padding-bottom: 1px;
+            margin-top: 20px;
+            margin-right: 30px;
+            text-align: center;
+            color: white;
+            font-size: 14px;
+            font-weight: normal;
+            font-family: 'Times New Roman', Times, serif;
+            background-color: steelblue;
+            border: 3px solid rgb(32, 106, 163);
+            border-radius: 8px;
+            text-decoration: none;
+        }
+
+
+        .result {
+            margin-top: 5px;
+        }
+
+        td {
+            text-align: left;
+            padding: 3px;
+        }
+
+        th {
+            font-weight: normal;
+            text-align: left;
+            padding: 3px;
+        }
+
+        .action {
+            color: white;
+            display: inline-table;
+            font-size: 12px;
+            padding-left: 10px;
+            padding-right: 10px;
+            padding-top: 2px;
+            padding-bottom: 2px;
+            background-color: rgb(125, 174, 216);
+            border: 2px solid steelblue;
+            text-decoration: none;
+            margin-right: 8px;
+        }
+    </style>    
 </head>
 
 <?php
@@ -19,24 +110,24 @@
 
     $database_name="test";
     $conn = mysqli_connect($server_name, $username, $password, $database_name);
-    $sql = "SELECT * FROM subject";
+    $sql = "SELECT * FROM subjects";
 
         $result = mysqli_query($conn, $sql);
         $course = array("" => "");
         while($row = mysqli_fetch_assoc($result)){
-            array_push($course, $row["name"]);
+            array_push($course, $row["school_year"]);
         }
         // if (isset($_GET["search"]) && !empty($_GET["search"])){
         //     $key = $_GET["search"];
-        //     $sql_search = "SELECT * FROM subject WHERE description LIKE '%key%' OR name LIKE '%key%' OR school_year LIKE '%key%'";
+        //     $sql_search = "SELECT * FROM subjects WHERE description LIKE '%key%' OR name LIKE '%key%' OR school_year LIKE '%key%'";
         // }else{
-        //     $sql_search = "SELECT * FROM subject";
+        //     $sql_search = "SELECT * FROM subjects";
         // }
         // $search_result = mysqli_query($conn, $sql_search);
         
 ?>
 <body>
-    <form method='get' action='../../app/controller/classroom_search.php' name=''>
+    <form method='get' action='lecture_search.php' name=''>
     <div class="fieldset" style = "width: 600px;
                                 height: 520px;
                                 padding-left: 15px;
@@ -76,10 +167,11 @@
             
         
         <?php
-            $search_string = "SELECT * FROM subject WHERE ";
+
+            $search_string = "SELECT * FROM subjects WHERE ";
             if  (isset($_GET['course']) ? $_GET['course'] : ''){
                 $search_key = trim($_GET['course']);
-                $search_string .= "name LIKE '%$search_key%'";
+                $search_string .= "school_year = '$search_key'";
                 $course_query = mysqli_query($conn, $search_string);
                 $result_count = mysqli_num_rows($course_query);
 
@@ -119,8 +211,8 @@
                                 <td>".$school_year."</td>
                                 <td>".$description."</td>
                                 <td>
-                                    <a class='action' href=''>Xóa</a>
-                                    <a class='action' href=''>Sửa</a>
+                                    <a class='action' onclick='myFunction()'>Xóa</a>
+                                    <a class='action' href='lecture_edit.php'>Sửa</a>
                                 </td>
                             </tr>";
                             
@@ -137,7 +229,7 @@
                 $key = trim($_GET['key']);
                 $display_words = "";
         
-                $search_string .= "description LIKE '%$key%' OR school_year LIKE '%$key%' OR name LIKE '%$key%'";
+                $search_string .= "description LIKE '%$key%' OR name LIKE '%$key%'";
         
                 // run the query in the db and search through each of the records returned
                 $query = mysqli_query($conn, $search_string);
@@ -179,8 +271,8 @@
                                 <td>".$school_year."</td>
                                 <td>".$description."</td>
                                 <td>
-                                    <a class='action' href=''>Xóa</a>
-                                    <a class='action' href=''>Sửa</a>
+                                    <a class='action' onclick='myFunction()'>Xóa</a>
+                                    <a class='action' href='lecture_edit.php'>Sửa</a>
                                 </td>
                             </tr>";
                             
@@ -193,28 +285,18 @@
             }
            
         ?>
-                   
-    <!-- <script type="text/javascript">
-        document.getElementById("department_in").value = getSavedValue("department_in");    // set the value to this input
-        document.getElementById("key_input").value = getSavedValue("key_input");   // set the value to this input
-        /* Here you can add more inputs to set value. if it's saved */
-
-        //Save the value function - save it to localStorage as (ID, VALUE)
-        function saveValue(e){
-            var id = e.id;  // get the sender's id to save it . 
-            var val = e.value; // get the value. 
-            localStorage.setItem(id, val);// Every time user writing something, the localStorage's value will override . 
-        }
-
-        //get the saved value function - return the value of "v" from localStorage. 
-        function getSavedValue  (v){
-            if (!localStorage.getItem(v)) {
-                return "";// You can change this to your defualt value. 
+        <script>
+            function myFunction() {
+                var x;
+                var r = confirm("Bấm Ok để xóa");
+                if (r == true) {
+                x = "You pressed OK!";
+                }
+                else {
+                x = "You pressed Cancel!";
+                }
+                document.getElementById("demo").innerHTML = x;
             }
-            return localStorage.getItem(v);
-        }
-    </script>
-
-    <script src="index.js"></script> -->
+        </script>          
 </body>
 </html>
